d70469fbc5d15ad95a96c22d54fe58510ee301b4
Dāvis Strazds <davisstrazds@gmail.com>
2026-02-16 00:26:28 +0200

    Pabeigts

--- Ieskaite_UI_DavisStrazds/Assets/Scripts/PalidzibasManager.cs
+++ Ieskaite_UI_DavisStrazds/Assets/Scripts/PalidzibasManager.cs
@@ -1,46 +1,88 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PalidzibasManager : MonoBehaviour
 {
     public TextMeshProUGUI sarakstaTeksts;
     public Image telaBilde;
     public Sprite raudBilde;
-    public Sprite smaidigaBilde; 
+    public Sprite smaidigaBilde;
+    public GameObject klientsObjekts;
 
-    void Start()
-    {
+    public GameObject segaObjekts;
+    public GameObject zupaObjekts;
+    public GameObject vitaminiObjekts;
+
+    public TMP_InputField vardaLauks;
+    public TMP_InputField gadaLauks;
+
+    void Start() {
         MainitTelu(0);
     }
 
     public void PogaPartika() {
-        sarakstaTeksts.text = "Pārtika: Zupa un maize sagatavota!";
+        sarakstaTeksts.text = "Pārtika: Zupa, maize un āboli sagatavoti!";
     }
 
     public void PogaMedicina() {
-        sarakstaTeksts.text = "Medicīna: Plāksteri un vitamīni pieejami.";
+        sarakstaTeksts.text = "Medicīna: Termometrs, plāksteri un zāles ir plauktā.";
     }
 
     public void PogaPsihologs() {
-        sarakstaTeksts.text = "Psihologs: Speciālists ir gatavs uzklausīt.";
+        sarakstaTeksts.text = "Psihologs: Ieteikta atpūta un tēja. Viss būs labi!";
     }
 
-    public void MainitTelu(int index)
-    {
+    public void AprekinatVecumu() {
+        if (vardaLauks.text != "" && gadaLauks.text != "") {
+            string vards = vardaLauks.text;
+            int gads = int.Parse(gadaLauks.text);
+            int vecums = 2026 - gads; 
+            sarakstaTeksts.text = vards + " ir " + vecums + " gadus vecs/a.";
+        }
+    }
+
+    public void ToggleSega(bool irIeslegts) {
+        if(segaObjekts != null) segaObjekts.SetActive(irIeslegts);
+    }
+
+    public void ToggleZupa(bool irIeslegts) {
+        if(zupaObjekts != null) zupaObjekts.SetActive(irIeslegts);
+    }
+
+    public void ToggleVitamini(bool irIeslegts) {
+        if(vitaminiObjekts != null) vitaminiObjekts.SetActive(irIeslegts);
+    }
+
+    public void MainitTelu(int index) {
         if (index == 0) {
             telaBilde.color = new Color(1, 1, 1, 0);
-            sarakstaTeksts.text = "Kā tu jūties?";
+            sarakstaTeksts.text = "Izvēlies tēlu!";
         } 
         else if (index == 1) {
             telaBilde.sprite = raudBilde;
             telaBilde.color = new Color(1, 1, 1, 1);
-            sarakstaTeksts.text = "Klients ir bēdīgs un viņam nepieciešama palīdzība.";
+            sarakstaTeksts.text = "Klients ir bēdīgs.";
         } 
         else if (index == 2) {
             telaBilde.sprite = smaidigaBilde;
             telaBilde.color = new Color(1, 1, 1, 1);
-            sarakstaTeksts.text = "Lieliski! Klients jūtas aprūpēts un smaidīgs.";
+            sarakstaTeksts.text = "Lieliski! Klients smaidīgs.";
         }
+    }
+
+    public void MainitPlatumu(float vertiba) {
+        if (klientsObjekts != null)
+            klientsObjekts.transform.localScale = new Vector3(vertiba, klientsObjekts.transform.localScale.y, 1f);
+    }
+
+    public void MainitGarumu(float vertiba) {
+        if (klientsObjekts != null)
+            klientsObjekts.transform.localScale = new Vector3(klientsObjekts.transform.localScale.x, vertiba, 1f);
+    }
+
+    public void IzietKoridora() {
+        SceneManager.LoadScene("SampleScene"); 
     }
 }
